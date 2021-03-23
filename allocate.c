c20cbfc0f18e1f8bc5f63b512fc85d08875f3ac7
Xueqi Guan <xueqig@student.unimelb.edu.au>
2021-03-23 12:36:39 +1100

    implement getopt

--- allocate.c
+++ allocate.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main()
+// typedef struct
+// {
+//     int arr_time;
+//     int id;
+//     int exe_time;
+//     char is_par;
+// } process_t;
+
+int main(int argc, char **argv)
 {
-    printf("Hello\n");
+    char *filename;
+    int option, proessors;
+
+    opterr = 0;
+
+    while ((option = getopt(argc, argv, "p:f:")) != -1)
+        switch (option)
+        {
+        case 'p':
+            proessors = atoi(optarg);
+            printf("proessors = %d\n", proessors);
+            break;
+        case 'f':
+            filename = optarg;
+            printf("filename = %s\n", filename);
+            break;
+        default:
+            return 0;
+        }
     return 0;
 }
